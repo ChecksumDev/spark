e82b7eb76b31aeb365cf4d548efa4e2db57ea174
Checksum <collierdevs@tuta.io>
2021-07-28 14:00:35 -0500

    update version to 1.2.0

--- spark.sh
+++ spark.sh
@@ -4,14 +4,14 @@
 #   \__ \/ __ \/ __ `/ ___/ //_/
 #  ___/ / /_/ / /_/ / /  / ,<
 # /____/ .___/\__,_/_/  /_/|_|
-#     /_/ v 1.1.0
+#     /_/ v 1.2.0
 #
 # Copyright (c) 2021 ChecksumDev.
 # Licensed under the GNU GPLv3.
 
 # globals
 COUNT=0
-VERSION=1.1.0
+VERSION=1.2.0
 DISTRO=$(lsb_release -sc)
 MIRRORS="$(wget -qO - mirrors.ubuntu.com/mirrors.txt)"
 
